ae536aa4764e2f97bad8fc38bb9ae03343736342
vinicius.santos <vinicius.santos@senior.com.br>
2024-07-16 22:47:10 -0300

    Adicionar testes Cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,6 @@
 
 cypress.Commands.add ('filMandatoryFieldsAndSubmit', function () {
 
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     cy.get('#firstName').type('Vinicius Henrique')
     cy.get('#lastName').type('Santos')
     cy.get('#email').type('teste.erp2023@gmail.com')
